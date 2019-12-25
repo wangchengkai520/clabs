3c0e195c65279c24c9c04800ae0cf5029e3c8c9f
wangchengkai520 <56427726+wangchengkai520@users.noreply.github.com>
2019-12-25 05:15:27 -0800

    Add files via upload

--- lab3/ex1.c
+++ lab3/ex1.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
-int strindex (char s[],char t[])
+#include<string.h>
+int strindex (char s[], char t[])
 {
-    int i,j,k;
-    for(i=0;s[i]!='\0';i++){
-        for(j=i,k=0;t[k]!='\0'&& s[j]==t[k];j++,k++);
-        if(k>0&&t[k]=='\0')
-        return i;    
+    int i,j,k,pos=0;
+    for(i = 0;s[i] !='\0'; i++){
+        for(j = i,k=0;t[k]!='\0'&& s[j]==t[k];j++,k++);
+        if(t[k]=='\0'&& k>0){
+        pos = i+1;
     }
+    }
+    if(pos >0)
+    return pos-1;
     return -1;
 }
-int main(){
-    char x[100];
-    char y[100];
+main(){
+    char a[100];
+    char b[100];
     printf("please input the chain of characters\n");
-    scanf("%s %s",x,y);
-    printf("%d\n",strindex(x,y));
+    scanf("%s %s", a,b);
+    printf("%d\n",strindex(b,a));
 }
